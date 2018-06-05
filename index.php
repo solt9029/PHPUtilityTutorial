eb92c169596657ef9738f0e7c48597534ea67612
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-06-05 18:27:21 +0900

    update static method

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 require __DIR__ . "/vendor/autoload.php";
 use Solt9029\Utility;
 
-$utility = new Utility();
 $positions = [[10, 10], [20, 10], [30, 30], [40, 40], [100, 100], [100, 120]];
-$circle = $utility->getCircleContainingAllPositions($positions);
+$circle = Utility::getCircleContainingAllPositions($positions);
 var_dump($circle);
